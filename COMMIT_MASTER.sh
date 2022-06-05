31dae5a7ad6dca84a7abb4fb35315777802e6ab7
KIVINSAEHOME\Kivinsae <kivinsae@gmail.com>
2022-06-05 23:04:44 +0800

    add((lc)): Submit [Q1, Q2]

---
+++ COMMIT_MASTER.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please input commit info in AngularJS form (<type>(topic): details):"
+read -p "Enter the type of commit:" type
+read -p "Enter the topic of commit:" topic
+read -p "Enter the details of commit:" details
+git add .
+git commit -m "$type($topic): $details"
+git push git@github.com:KKtheGhost/Kivinsae-LeetCode-C.git master
---
+++ README.md
@@ -0,0 +1,6 @@
+## Kivinsae LeetCode - C
+
+未来会根据自己变成语言的认知进度，使用不同语言刷LeetCode。
+- 苦心人，天不负，卧薪尝胆，三千越甲可吞吴。
+- `Link_Gen.sh` & `Link_Gen.PS1`会将 All 目录中的提交结果自动分类并软链到 Difficulty 目录下的对应难度目录里。
+- `COMMIT_MASTER` 为符合 `AngularJS` 的便利提交脚本。
