2d3c2d4278849a9ae23f84e4e4b3385c5f0332b5
KKtheGhost <kivinsae@gmail.com>
2022-06-06 16:51:45 +0800

    modify(script): add link_gen.sh into commit_master.sh

--- COMMIT_MASTER.sh
+++ COMMIT_MASTER.sh
@@ -4,6 +4,8 @@ echo "Please input commit info in AngularJS form (<type>(topic): details):"
 read -p "Enter the type of commit:" type
 read -p "Enter the topic of commit:" topic
 read -p "Enter the details of commit:" details
+
+./Link_Gen.sh
 git add .
 git commit -m "$type($topic): $details"
 git push git@github.com:KKtheGhost/Kivinsae-LeetCode-C.git main
---
+++ Difficulty/Medium/3.longest-substring-without-repeating-characters2.c
@@ -0,0 +1,1 @@
+../../All/3.longest-substring-without-repeating-characters2.c
