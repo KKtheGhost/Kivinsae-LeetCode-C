9ff2ef67ed077db9f521a2f64d8fa064fc35fb09
KKtheGhost <kivinsae@gmail.com>
2022-06-28 13:18:13 +0000

    :boom:(CICD) Auto-Gen Difficulty Directory

--- Difficulty/Medium/9.palindrome-number.c
+++ Difficulty/Medium/9.palindrome-number.c
@@ -1,17 +1,1 @@
-/*
- * @lc app=leetcode id=9 lang=c
- *
- * [9] Palindrome Number
- */
-
-// @lc code=start
-
-
-bool isPalindrome(int x){
-    if (x < 0) {return 0;}
-    long long rev = 0, org = x;
-    while (x != 0) {rev = rev*10 + x%10; x /= 10;}
-    return (org == rev)? 1 : 0;
-}
-// @lc code=end
-// @lc MEDIUM
+./All/9.palindrome-number.c
